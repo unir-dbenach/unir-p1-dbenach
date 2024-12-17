3378c5dea3431d136b9c6e94c0f47272bfdf9960
unir-dbenach <davidbenach@yahoo.es>
2024-12-17 16:44:38 +0100

    modificación de la api y res_test

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -4,6 +4,7 @@ import unittest
 from urllib.request import urlopen
 
 import pytest
+from requests.exceptions import HTTPError
 
 BASE_URL = "http://192.168.71.10:5000"
 BASE_URL_MOCK = "http://192.168.71.10:9090"
@@ -24,6 +25,35 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "3", "ERROR ADD"
         )
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/7"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "21", "ERROR MULTIPLY"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/21/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "7.0", "ERROR DIVIDE"
+        )
+
+    def test_api_divideByZero(self):
+        url = f"{BASE_URL}/calc/divide/21/0"
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        except Exception as e:
+            self.assertEqual(
+                    e.read().decode(), "406", "ERROR DIVIDEBYZERO"
+            )
 
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
